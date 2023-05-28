8c4e476b6b664003f986ccd2e9bcdec197710013
Bogdan Drema <zurnaz@gmail.com>
2023-05-29 00:34:58 +0100

    fix: remove deprecated multimap no longer present

--- mtj_softtuner/patch.py
+++ mtj_softtuner/patch.py
@@ -51,13 +51,6 @@ def patch(f: __F) -> __F:
         # as per https://github.com/deepmind/dm-haiku/issues/191
         os.environ["HAIKU_FLATMAPPING"] = "0"
 
-        # In JAX 0.2.13, jax.tree_multimap was renamed to jax.tree_map and
-        # jax.tree_multimap became an alias of this new jax.tree_map,
-        # and optax depends on this change, so we're shimming jax.tree_map calls to
-        # go to the current jax.tree_multimap (which is the same as the JAX 0.2.13
-        # versions of both jax.tree_map and jax.tree_multimap) for compatibility
-        # with optax.
-        jax.tree_map = jax.tree_multimap
 
         # Colab doesn't seem to like ReplicatedLayerNorm, so we're just going to use
         # haiku's standard LayerNorm modules, which we can do because we aren't going
